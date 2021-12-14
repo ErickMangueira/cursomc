2eaa0ddf234f06bcf1920df4971a2bef1398e275
Erick Mangueira <erickmangueira@gmail.com>
2021-12-14 13:36:56 -0300

    Validacao customizada email nao repetido na atualizacao de Cliente

--- src/main/java/com/example/cursomc/services/validation/ClienteUpdate.java
+++ src/main/java/com/example/cursomc/services/validation/ClienteUpdate.java
@@ -1,4 +1,3 @@
-
 package com.example.cursomc.services.validation;
 
 import java.lang.annotation.ElementType;
@@ -8,11 +7,11 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Constraint(validatedBy = ClienteInsertValidator.class)
+@Constraint(validatedBy = ClienteUpdateValidator.class)
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 
-public @interface ClienteInsert {
+public @interface ClienteUpdate {
 	String message() default "Erro de validação";
 
 	Class<?>[] groups() default {};
