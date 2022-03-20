6ef2c1ed9e73a7ee7d03b4faa41b9c3bb7acc3ec
Erick Mangueira <erickmangueira@gmail.com>
2022-03-20 10:10:36 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/example/cursomc/dto/CidadeDTO.java
@@ -0,0 +1,42 @@
+package com.example.cursomc.dto;
+
+import com.example.cursomc.domain.Cidade;
+
+public class CidadeDTO {
+	
+	private Integer id;
+	private String nome;
+	
+	
+	public CidadeDTO() {
+		
+	}
+
+
+	public CidadeDTO(Cidade obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
